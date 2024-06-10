aedaed7766e4be7fc2ec8d0fa277733d6f997e05
Arthur <dss.arthur0@gmail.com>
2024-06-10 19:33:12 -0300

    first commit

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-41.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
+	UAGBTimelineClasses( {"block_id":"4e90c9ed","timelinAlignment":"center","timelinAlignmentTablet":"left","timelinAlignmentMobile":"left"}, '.uagb-block-4e90c9ed' );
+});
+window.addEventListener("resize", function(){
+	UAGBTimelineClasses( {"block_id":"4e90c9ed","timelinAlignment":"center","timelinAlignmentTablet":"left","timelinAlignmentMobile":"left"}, '.uagb-block-4e90c9ed' );
+});
+ });
---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-86.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-aaa04ba3' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-0b244cea' );
+});
+ });
