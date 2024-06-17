b1e36076acb324de5920dc6cc85a9670d1fe804a
Arthur <dss.arthur0@gmail.com>
2024-06-17 11:12:25 -0300

    Correção de bugs na versao mobile do site

--- wp-content/uploads/uag-plugin/assets/0/uag-js-41.js
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-41.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
-	UAGBTimelineClasses( {"block_id":"4e90c9ed","timelinAlignment":"center","timelinAlignmentTablet":"left","timelinAlignmentMobile":"left"}, '.uagb-block-4e90c9ed' );
+	UAGBTimelineClasses( {"block_id":"cb8ae62e","timelinAlignment":"center","timelinAlignmentTablet":"left","timelinAlignmentMobile":"left"}, '.uagb-block-cb8ae62e' );
 });
 window.addEventListener("resize", function(){
-	UAGBTimelineClasses( {"block_id":"4e90c9ed","timelinAlignment":"center","timelinAlignmentTablet":"left","timelinAlignmentMobile":"left"}, '.uagb-block-4e90c9ed' );
+	UAGBTimelineClasses( {"block_id":"cb8ae62e","timelinAlignment":"center","timelinAlignmentTablet":"left","timelinAlignmentMobile":"left"}, '.uagb-block-cb8ae62e' );
 });
  });
